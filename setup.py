f227ba108cf6e2e61094d321f5b56a2422d8f004
Divyanshu Rastogi <divyanshur@maqsoftware.com>
2025-10-29 11:37:39 +0530

    updae

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="maq-rai-sdk_test",
-    version="0.3.8",
+    version="0.3.9",
     description="RAI Package contains a Prompt Reviewer and Updater and test case generator for faster Copilot Development",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
